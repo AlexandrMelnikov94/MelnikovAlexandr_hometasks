d52f10b7edca26875bb5671b8eca6693dca80177
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2021-05-31 14:27:03 +0300

    7 point for hometask

--- lesson-7-oop/script.js
+++ lesson-7-oop/script.js
@@ -1,32 +1,34 @@
-function Question(question, answers, correctAnswer) {
-    this.question = question;
-    this.answers = answers;
-    this.correctAnswer = correctAnswer;
-}
-
-Question.prototype.showQuestion = function () {
-    console.log(this.question);
-
-    for (var i = 0; i < this.answers.length; i++) {
-        console.log(i + ': ' + this.answers[i]);
+(function() {
+    function Question(question, answers, correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
     }
-}
 
-Question.prototype.chekAnswer = function (answer) {
-    if (answer === this.correctAnswer) {
-        console.log('Your are right!!!');
-    } else {
-        console.log('Wrong answer!');
+    Question.prototype.showQuestion = function () {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
     }
-}
 
-var questionOne = new Question('Were are yo live?', ['Grondo', 'Minsk', 'Gomel', 'Vitebsk'], 0);
-var questionTwo = new Question('Capital of GB?', ['Moscow', 'Minsk', 'London', 'Paris'], 2);
-var questionThree = new Question('Capital of USA?', ['Moscow', 'London', 'NewYork', 'Washington'], 3);
+    Question.prototype.chekAnswer = function (answer) {
+        if (answer === this.correctAnswer) {
+            console.log('Your are right!!!');
+        } else {
+            console.log('Wrong answer!');
+        }
+    }
 
-var questionsPull = [questionOne, questionTwo, questionThree];
-var whatQuestion = Math.floor(Math.random() * questionsPull.length);
-questionsPull[whatQuestion].showQuestion();
+    var questionOne = new Question('Were are yo live?', ['Grondo', 'Minsk', 'Gomel', 'Vitebsk'], 0);
+    var questionTwo = new Question('Capital of GB?', ['Moscow', 'Minsk', 'London', 'Paris'], 2);
+    var questionThree = new Question('Capital of USA?', ['Moscow', 'London', 'NewYork', 'Washington'], 3);
 
-var yourAnswer = parseInt(prompt('Choose answer'));
-questionsPull[whatQuestion].chekAnswer(yourAnswer);
+    var questionsPull = [questionOne, questionTwo, questionThree];
+    var whatQuestion = Math.floor(Math.random() * questionsPull.length);
+    questionsPull[whatQuestion].showQuestion();
+
+    var yourAnswer = parseInt(prompt('Choose answer'));
+    questionsPull[whatQuestion].chekAnswer(yourAnswer);
+})();

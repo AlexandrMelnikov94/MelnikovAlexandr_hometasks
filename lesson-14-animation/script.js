71b1c7c1813eaf3ca065631a2a25b3ce8d8f29fa
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2021-06-30 16:15:18 +0300

    add players for game

--- lesson-14-animation/script.js
+++ lesson-14-animation/script.js
@@ -1,109 +1,128 @@
-var fieldWidth = 1000;
-var fieldHeight = 700;
-var ballWidth = 50;
-var ballHeight = 50;
-var rocketHeight = 150;
+(function () {
+  var fieldWidth = 1000;
+  var fieldHeight = 700;
+  var ballWidth = 50;
+  var ballHeight = 50;
+  var rocketHeight = 150;
 
-var RequestAnimationFrame =
-  window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.oRequestAnimationFrame ||
-  window.msRequestAnimationFrame ||
+  var startButton = document.getElementById('start');
+  var leftPlayer = document.getElementById('left-player');
+  var rightPlayer = document.getElementById('right-player');
 
-  function (callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
+  var leftPlayerRocket = new Player(leftPlayer);
+  var rightPlayerRocket = new Player(rightPlayer);
 
-var Ball = {
-  PosX: fieldWidth / 2 - ballWidth / 2,
-  PosY: fieldHeight / 2 - ballHeight / 2,
-  SpeedX: ((Math.floor(Math.random() * (1 - (-1) + 1)) + (-1)) * 3) + 6,
-  SpeedY: ((Math.floor(Math.random() * (1 - (-1) + 1)) + (-1)) * 3) + 4,
-  Width: ballWidth,
-  Height: ballHeight,
+  var RequestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
 
-  Update: function () {
-    var BallObj = document.getElementById('ball');
-    BallObj.style.left = this.PosX + 'px';
-    BallObj.style.top = this.PosY + 'px';
-  }
-}
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
 
-var Area = {
-  Width: fieldWidth,
-  Height: fieldHeight,
-}
-
-function Start() {
- RequestAnimationFrame(Tick);
-}
-
-function Tick() {
-  //рокетка
-  // PlayerLeft.update();
-  // PlayerLeft.PosY += PlayerLeft.SpeedY;
-  //мячик
-  Ball.PosX += Ball.SpeedX;
-  if (Ball.PosX + Ball.Width > Area.Width) {
-    Ball.SpeedX = - Ball.SpeedX;
-  }
-  if (Ball.PosX < 0) {
-    Ball.SpeedX = -Ball.SpeedX;
-    Ball.PosX = 0;
+// РОКЕТКА -----------------------------------------------------------------------------------------------------------
+  function Player(player) {
+    this.speedY = 0;
+    player.style.top = fieldHeight / 2 - rocketHeight / 2 + 'px';
+    this.Update = function () {
+      player.style.top = player.offsetTop + this.speedY + 'px';
+      if (player.offsetTop <= 0) {
+        player.style.top = '0px';
+      } else if (player.offsetTop >= fieldHeight - rocketHeight){
+        player.style.top = fieldHeight - rocketHeight + 'px';
+      }
+    }
   }
 
-  Ball.PosY += Ball.SpeedY;
-  if (Ball.PosY + Ball.Height > Area.Height) {
-    Ball.SpeedY = - Ball.SpeedY;
-    Ball.PosY = Area.Height -Ball.Height;
+  function moveRocket(e) {
+    if (e.keyCode === 16) {
+      leftPlayerRocket.speedY = -5;
+    }
+    if (e.keyCode === 17) {
+      leftPlayerRocket.speedY = 5;
+    }
+    if (e.keyCode === 38) {
+      rightPlayerRocket.speedY = -5;
+    }
+    if (e.keyCode === 40) {
+      rightPlayerRocket.speedY = 5;
+    }
   }
 
-  if (Ball.PosY < 0) {
-    Ball.SpeedY = - Ball.SpeedY;
-    Ball.PosY = 0;
+  function stopMoveRocket() {
+    leftPlayerRocket.speedY = 0;
+    rightPlayerRocket.speedY = 0;
+  }
+
+//МЯЧИК --------------------------------------------------------------------------------------------------------------
+  var Ball = {
+    PosX: fieldWidth / 2 - ballWidth / 2,
+    PosY: fieldHeight / 2 - ballHeight / 2,
+    SpeedX: ((Math.floor(Math.random() * (1 - (-1) + 1)) + (-1)) * 3) + 6,
+    SpeedY: ((Math.floor(Math.random() * (1 - (-1) + 1)) + (-1)) * 3) + 4,
+    Width: ballWidth,
+    Height: ballHeight,
+
+    Update: function () {
+      var BallObj = document.getElementById('ball');
+      BallObj.style.left = this.PosX + 'px';
+      BallObj.style.top = this.PosY + 'px';
+    }
+  }
+
+  var Area = {
+    Width: fieldWidth,
+    Height: fieldHeight,
+  }
+
+  // ФУНКЦИОНАЛ ----------------------------------------------------------------------------------------------------
+  function Start() {
+    leftPlayerRocket.Update();
+    rightPlayerRocket.Update();
+    RequestAnimationFrame(Start);
+  }
+
+  function StartBall() {
+    RequestAnimationFrame(Tick)
+  }
+
+  function initGame() {
+    RequestAnimationFrame(Start);
+  }
+
+  function Tick() {
+    Ball.PosX += Ball.SpeedX;
+    if (Ball.PosX + Ball.Width > Area.Width) {
+      Ball.SpeedX = -Ball.SpeedX;
+    }
+    if (Ball.PosX < 0) {
+      Ball.SpeedX = -Ball.SpeedX;
+      Ball.PosX = 0;
+    }
+
+    Ball.PosY += Ball.SpeedY;
+    if (Ball.PosY + Ball.Height > Area.Height) {
+      Ball.SpeedY = -Ball.SpeedY;
+      Ball.PosY = Area.Height - Ball.Height;
+    }
+
+    if (Ball.PosY < 0) {
+      Ball.SpeedY = -Ball.SpeedY;
+      Ball.PosY = 0;
+    }
+
+    Ball.Update();
+
+    RequestAnimationFrame(Tick);
   }
 
   Ball.Update();
 
-  RequestAnimationFrame(Tick);
-}
-
-Ball.Update();
-
-var leftRocketElement = document.getElementById('left-player');
-
-PlayerLeft = {
-  PosY: fieldHeight / 2 - rocketHeight /2,
-  SpeedY : 5,
-
-  Update: function () {
-    leftRocketElement.style.top = this.PosY + 'px';
-  }
-}
-
-PlayerLeft.Update();
-
-window.addEventListener('keydown' , function (e) {
-  e = e || window.event;
-  e.preventDefault();
-  if (e.keyCode === 16) {
-    PlayerLeft.speedY = -5;
-  }
-  if (e.keyCode === 17) {
-    PlayerLeft.speedY = 5;
-  }
-});
-// function moveLeftRocket(e) {
-//   e = e || window.event;
-//   e.preventDefault();
-//   if (e.keyCode === 16) {
-//     PlayerLeft.speedY = -5;
-//   }
-//   if (e.keyCode === 17) {
-//     PlayerLeft.speedY = 5;
-//   }
-// }
-
-
-// window.onkeydown = moveLeftRocket;
+  startButton.onclick = StartBall;
+  window.onload = initGame;
+  window.onkeydown = moveRocket;
+  window.onkeyup = stopMoveRocket;
+}());
